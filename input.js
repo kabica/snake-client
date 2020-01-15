9a30f01175f85ad3d9b73d4f1fedd8cae3a696b1
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-15 21:33:12 +0000

    input module added to repo

---
+++ input.js
@@ -0,0 +1,18 @@
+// input.js
+const handleUserInput = (key) => {
+	if(key === '\u0003') process.exit();
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data' , (data) => {
+  	return handleUserInput(data);
+  });
+}
+
+
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,24 +1,9 @@
-//const net = require('net');
 const { connect } = require('./client');
-const stdin = process.stdin;
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = (key) => {
-	if(key === '\u0003') process.exit();
-}
-
-stdin.on('data' , data => {
-	return handleUserInput(data);
-})
 
 setupInput();
