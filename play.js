eb0318255e5ad873528fdcbee5aa36e16a283aca
Kevin Alexander <kalex.bica@gmail.com>
2020-01-15 11:37:48 -0700

    client side code added to connect to snek-multiplayer server

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 3000 //50541
+    //192.168.168.76
+    //10.0.2.15
+  });
+
+  conn.on('data', (data) => {
+  	console.log('Server says: ' , data);
+  });
+
+  conn.on('connect', () => {
+  conn.write('Hello from client!');
+});
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
