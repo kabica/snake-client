260e1933441620b306f61a8181af394e71d60c52
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-15 18:56:34 +0000

    client file added to separate the connection logic into own module

--- play.js
+++ play.js
@@ -1,28 +1,11 @@
-const net = require('net');
-
+//const net = require('net');
+const { connect } = require('./client');
+console.log('Connecting ...');
+connect();
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 3000 //50541
-    //192.168.168.76
-    //10.0.2.15
-  });
 
-  conn.on('data', (data) => {
-  	console.log('Server says: ' , data);
-  });
 
-  conn.on('connect', () => {
-  conn.write('Hello from client!');
-});
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
-console.log('Connecting ...');
-connect();
+//console.log('Connecting ...');
+//connect();
