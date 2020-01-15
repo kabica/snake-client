8ab884e540a8410e492a2dbc654022a68a5a1176
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-15 21:17:33 +0000

    ctrl+c functionality added to client side

--- play.js
+++ play.js
@@ -1,11 +1,24 @@
 //const net = require('net');
 const { connect } = require('./client');
+const stdin = process.stdin;
 console.log('Connecting ...');
 connect();
-/**
- * Establishes connection with the game server
- */
 
 
-//console.log('Connecting ...');
-//connect();
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+	if(key === '\u0003') process.exit();
+}
+
+stdin.on('data' , data => {
+	return handleUserInput(data);
+})
+
+setupInput();
